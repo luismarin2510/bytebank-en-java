7e8c93863af0d8009b34ebe93624828de2d7a34a
Luismarin2510 <opraedu@gmail.com>
2023-02-21 21:14:56 -0500

    ejecucion de test

--- src/test_funcionario.java
+++ src/test_funcionario.java
@@ -2,7 +2,12 @@
 public class test_funcionario {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Funcionario Luis = new Funcionario();
+		Luis.setNombre("Luis Marin");
+		Luis.setDocumento("1099368");
+		Luis.setSalario(250);
+		System.out.println(Luis.getNombre() + " salario " + Luis.getSalario());
 
 	}
 
