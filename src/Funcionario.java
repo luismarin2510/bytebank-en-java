bc2d8d97e63798f93346349871a8908fa3bd0801
Luismarin2510 <opraedu@gmail.com>
2023-02-21 21:07:01 -0500

    metodos get y set

--- src/Funcionario.java
+++ src/Funcionario.java
@@ -5,5 +5,26 @@ public class Funcionario {
 	private String documento;
 	private double salario;
 	
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDocumento() {
+		return documento;
+	}
+	public void setDocumento(String documento) {
+		this.documento = documento;
+	}
+	public double getSalario() {
+		return salario;
+	}
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+	
+	
 
 }
